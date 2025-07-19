752fdad59a64434f391b14b8f6c425ec838731fd
hossein <149826811+hossein1dr@users.noreply.github.com>
2025-07-19 11:02:02 +0330

    add wow validation

---
+++ wow/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+namespace wow
+{
+
+    internal class Program
+    {
+        /// Functions:
+        /// 
+        /// Task 5:
+        /// Learn how to write documentation for a function.
+        /// 
+        /// function name: stringRepeater(String str, int repeat);
+        /// 
+        /// Implement both void and returnable functions.
+        static void wow_void(int num)
+        {
+
+            string x = 'w' + new string('o', num) + 'w';
+            Console.WriteLine(x);
+
+        }
+        static string wow_returnable(int num)
+        {
+
+
+            return 'w' + new string('o', num) + 'w';
+
+        }
+        static void Main(string[] args)
+        {
+            int y;
+            Console.WriteLine("pls enter number");
+            y = int.Parse(Console.ReadLine());
+
+            wow_void(y);
+
+            string result = wow_returnable(y);
+            Console.WriteLine("wow_returnable: {0}", result);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
+
+
+
---
+++ wow/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("wow")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("wow")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("74739da1-a398-4bf6-9a4b-9a38be6799c5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
